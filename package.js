1f0fbfc6ec6cba51665c3e502c2fe72c14a56889
Jonatan Littke <jonatan.littke@gmail.com>
2013-06-11 13:35:35 +0200

    Add Meteor packaging.

---
+++ package.js
@@ -0,0 +1,8 @@
+Package.describe({
+  summary: "Bootstrap from Twitter, custom packaged to allow individual JS files to be selected and to use mixins from the less files."
+});
+
+Package.on_use(function (api) {
+  api.add_files('js/bootstrap-dropdown.js', 'client');
+  api.add_files('js/bootstrap-collapse.js', 'client');
+});
